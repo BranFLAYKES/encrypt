e789cf59f2e94e038f95de390a982688abb4b80f
Brandon Figueredo <figueredobrandon@Brandons-MacBook-Pro.local>
2017-05-02 13:08:36 -0400

    Initial Commit

---
+++ encrypt/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  encrypt
+//
+//  Created by Brandon Figueredo on 5/2/17.
+//  Copyright © 2017 Brandon Figueredo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
