140b54f89c73805c70949ce509ce6f3cd1ec0430
Brandon Figueredo <figueredobrandon@Brandons-MacBook-Pro.local>
2017-05-04 13:33:33 -0400

    Finally...

--- encrypt/main.c
+++ encrypt/main.c
@@ -9,24 +9,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-#include <math.h>
 
-
+//unsigned char
 typedef unsigned  char Byte;
 
-void readFile(Byte* txtFile, FILE * output, unsigned long length,  const char * a);
+//prototype
 void displayBits(Byte value, unsigned long length, FILE * output);
 
 int main(int argc,  const char * argv[]) {
 	
-	FILE * fp;
-	FILE * output;
+    //declare file pointers/variables
+	FILE * fp = NULL;
+	FILE * output = NULL;
 	Byte * txtFile = (Byte*)malloc(sizeof(Byte));
+    Byte result;
 	int i = 0;
 	unsigned long length = 0;
 	
 	if (argc > 1)
 	{
+        //open files
 		fp = fopen(argv[1], "r+");
 		output = fopen(argv[2], "w+");
 	 
@@ -35,86 +37,63 @@ int main(int argc,  const char * argv[]) {
 			puts("File failed to open!");
 			exit(1);
 		}
-		
-		while ( !feof(fp))
-		{
-		
-			*(txtFile + i) = fgetc(fp);
-			i++;
-		}
-		
-		
-		
-		length = strlen((char*)txtFile);
-		
-			//don't really need this to print
-		/*
-		for (int a = 0; a < length; a++)
-		{
-			printf("%c", *(txtFile + a));
-		}
-		 */
-		
-		readFile(txtFile, output, length, argv[2]);
-		
-		fclose(fp);
-		
 	}
-	
-	free(txtFile);
-	
-	
+    
+    //store each character
+    while (!feof(fp))
+    {
+        *(txtFile + i) = fgetc(fp);
+        i++;
+    }
+    
+    //make length the size of file
+    length = strlen((char*)txtFile);
+    
+    puts("Original Text\n-----------------------------------------------");
+    
+    for (int a = 0; a < length; a++)
+    {
+        printf("%c", *(txtFile + a));
+    }
+    puts("Shifted Binary\t\t1's Complement Of Shift\n");
+    
+    for (int a = 0; a < length; a++)
+    {
+        //manipulate bits with bitwise OR and set the value equal to result
+        result = ( *(txtFile + a) << 2) | (*(txtFile + a) >> (6));
+
+        //print binary of shifted bits
+        for (int s= 7; s >= 0; --s)
+        {
+            putchar( (result & (1 << s)) ? '1' : '0' );
+        }
+        //calls function that prints final value in bits
+        printf("%s", "------->");
+        displayBits(~result, length, output);
+        puts("");
+    }
+
+    //close files
+    fclose(fp);
+    fclose(output);
+    free(txtFile);
+
 	return 0;
 }
 
- void readFile(Byte * txtFile, FILE * output, unsigned long length, const char * a)
- {
-	
-	 unsigned int mask = 1 << 31;
-	 unsigned int c;
-	 Byte result = '\0';
-
-	//print characters in binary
-	 for (int a = 0; a < length; a++)
-	 {
-		
-		 c = *(txtFile + a) << 24;
-		
-		for (int i = 0; i < 8; i++)
-		 {
-			putchar( c & mask ? '1' : '0');
-			c <<= 1;
-		}
-		 puts("\n\n");
-	}
-
-	
-		 //relocate bits
-	 for (int a = 0; a < length; a++)
-	 {
-		 
-		 for (int s= 7; s >= 0; --s)
-		 {
-			 result = *(txtFile + a) * (Byte)pow(2, 2);
-			 putchar( (result & (1 << s)) ? '1' : '0' );
-		 }
-		 printf("%s", "------->");
-		 displayBits(~result, length, output);
-		 puts("");
-	 }
-	 fclose(output);
- }
-	 
+//display complement of shifted bits
 void displayBits(Byte value, unsigned long length, FILE * output)
 {
-	Byte mask = 1 << 7;
+    //output char to file
 	fprintf(output, "%c", value);
 	
-	for (int c = 0; c < 8; ++c) {
-			putchar(value & mask ? '1' : '0');
-			value <<= 1;
+    //print binary of complemented value
+	for (int c = 7; c >= 0; --c) {
+			putchar(value & (1 << c) ? '1' : '0');
 			
 		}
 }
+
+
 
 
